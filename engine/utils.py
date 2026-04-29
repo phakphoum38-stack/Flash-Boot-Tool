4dc2852980b374a07f8f0e07b659773d469be329
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-30 03:17:20 +0700

    Update utils.py

--- engine/utils.py
+++ engine/utils.py
@@ -4,3 +4,6 @@ def sanitize_path(path):
     if ".." in path or path.startswith("/etc"):
         raise Exception("Invalid path")
     return os.path.abspath(path)
+
+def get_file_size(path):
+    return os.path.getsize(path)
