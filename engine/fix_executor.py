533d3c046b2b9c755378b6d9bce9bc29258cbabe
phakphoum38-stack <phakphoum38@gmail.com>
2026-04-30 04:53:46 +0700

    Update fix_executor.py

--- engine/fix_executor.py
+++ engine/fix_executor.py
@@ -37,3 +37,27 @@ def split_wim(wim_path, usb):
 
 def full_reset(device):
     subprocess.run(["mkfs.vfat", device])
+
+
+def rebuild_partition_table(device):
+    subprocess.run(["parted", device, "mklabel", "msdos"])
+
+def repair_grub(device):
+    subprocess.run(["grub-install", device])
+
+def rewrite_mbr(device):
+    subprocess.run(["dd", "if=/usr/lib/syslinux/mbr/mbr.bin", f"of={device}"])
+
+def windows_boot_fix(device):
+    return "Use Windows recovery (bootrec /fixmbr)"
+
+def safe_mode_boot(device):
+    return "Try boot with safe mode kernel"
+
+FIX_MAP = {
+    "rebuild_partition_table": rebuild_partition_table,
+    "repair_grub": repair_grub,
+    "rewrite_mbr": rewrite_mbr,
+    "windows_boot_fix": windows_boot_fix,
+    "safe_mode_boot": safe_mode_boot,
+}
